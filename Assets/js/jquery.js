77a9760a7d7535638c394940ddfcb47399bbef85
Antoninglz33 <antonin33gonzalez@gmail.com>
2021-08-02 16:56:09 +0200

    ajout skills bar

--- Assets/js/jquery.js
+++ Assets/js/jquery.js
@@ -2,7 +2,7 @@ $(function () {
   $(".background").mouseenter(function () {
     $("#skill-per-1").animate(
       {
-        width: "90%",
+        width: "100%",
       },
       1000
     );
@@ -27,6 +27,22 @@ $(function () {
     $("#skill-per-4").animate(
       {
         width: "60%",
+      },
+      1000
+    );
+  });
+  $(".background").mouseenter(function () {
+    $("#skill-per-5").animate(
+      {
+        width: "70%",
+      },
+      1000
+    );
+  });
+  $(".background").mouseenter(function () {
+    $("#skill-per-6").animate(
+      {
+        width: "80%",
       },
       1000
     );
