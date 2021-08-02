80ff1091cafa5326277d1ef75f1d240590ffce1d
Antoninglz33 <antonin33gonzalez@gmail.com>
2021-08-02 16:44:55 +0200

    skillsbar modif

--- Assets/js/jquery.js
+++ Assets/js/jquery.js
@@ -1,22 +1,34 @@
-$(function(){
-    $(".background").mouseenter(function(){
-        $("#skill-per-1").animate({
-            width: '70%'
-        },1000);
-    });
-    $(".background").mouseenter(function(){
-        $("#skill-per-2").animate({
-            width: '60%'
-        },1000);
-    });
-    $(".background").mouseenter(function(){
-        $("#skill-per-3").animate({
-            width: '20%'
-        },1000);
-    });
-    $(".background").mouseenter(function(){
-        $("#skill-per-4").animate({
-            width: '10%'
-        },1000);
-    });
+$(function () {
+  $(".background").mouseenter(function () {
+    $("#skill-per-1").animate(
+      {
+        width: "90%",
+      },
+      1000
+    );
+  });
+  $(".background").mouseenter(function () {
+    $("#skill-per-2").animate(
+      {
+        width: "90%",
+      },
+      1000
+    );
+  });
+  $(".background").mouseenter(function () {
+    $("#skill-per-3").animate(
+      {
+        width: "60%",
+      },
+      1000
+    );
+  });
+  $(".background").mouseenter(function () {
+    $("#skill-per-4").animate(
+      {
+        width: "60%",
+      },
+      1000
+    );
+  });
 });
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,35 +1,34 @@
-const ratio = .1
+const ratio = 0.1;
 const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: ratio
-}
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
 
 const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (entry.intersectionRatio > ratio) {
-            entry.target.classList.remove('reveal')
-            observer.unobserve(entry.target)
-        }
-    });
-}
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.remove("reveal");
+      observer.unobserve(entry.target);
+    }
+  });
+};
 
 const observer = new IntersectionObserver(handleIntersect, options);
-document.querySelectorAll('.reveal').forEach(function(r) {
-    observer.observe(r)
+document.querySelectorAll(".reveal").forEach(function (r) {
+  observer.observe(r);
 });
 
 /**************************************************
  * *****************  PARALLAX ********************
  * ***********************************************/
- 
 
-document.addEventListener("scroll", (e) => {
-    let scroll_pos = window.scrollY;
-    const head = document.querySelector("header");
-  
-    console.log(scroll_pos);
-    scroll_pos = scroll_pos * -3 * 0.5;
-  
-    head.style.backgroundPosition = "0px " + scroll_pos + "px";
-  });
+// document.addEventListener("scroll", (e) => {
+//     let scroll_pos = window.scrollY;
+//     const head = document.querySelector("header");
+
+//     console.log(scroll_pos);
+//     scroll_pos = scroll_pos * -3 * 0.5;
+
+//     head.style.backgroundPosition = "0px " + scroll_pos + "px";
+// });
